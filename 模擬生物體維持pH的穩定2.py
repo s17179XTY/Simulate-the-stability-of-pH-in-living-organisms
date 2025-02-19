c3a6f5f3f16c06c542eb765814153ca1b56c9427
Aicee <XTY17179@gmail.com>
2025-02-19 23:41:57 +0800

    Update 模擬生物體維持pH的穩定2.py

--- 模擬生物體維持pH的穩定2.py
+++ 模擬生物體維持pH的穩定2.py
@@ -12,9 +12,17 @@ y_axis_data2 = [ 7.2, 7.5, 7.7, 7.7, 7.8 , 8.0, 8.0]
 y_axis_data3 = [ 6.9, 7.0, 7.2, 7.7, 8.0, 8.3, 8.6]
 
 
-plt.plot(x_axis_data, y_axis_data1, 'bo-', alpha=0.5, linewidth=1, label='自來水')
-plt.plot(x_axis_data, y_axis_data2, 'ro-', alpha=0.5, linewidth=1, label='緩衝液')
-plt.plot(x_axis_data, y_axis_data3, 'go-', alpha=0.5, linewidth=1, label='薯仔水')
+plt.plot(x_axis_data, y_axis_data1, 'bo-', alpha=0.5, linewidth=1, label='自來水', color="blue")
+plt.plot(x_axis_data, y_axis_data2, 'ro-', alpha=0.5, linewidth=1, label='緩衝液', color="red")
+plt.plot(x_axis_data, y_axis_data3, 'go-', alpha=0.5, linewidth=1, label='薯仔水', color="green")
+
+
+for a, b in zip(x_axis_data, y_axis_data1):
+    plt.text(a, b, str(b), ha='center', va='bottom', fontsize=8, color="blue")
+for a, b1 in zip(x_axis_data, y_axis_data2):
+    plt.text(a, b1, str(b1), ha='center', va='bottom', fontsize=8, color="red") 
+for a, b2 in zip(x_axis_data, y_axis_data3):
+    plt.text(a, b2, str(b2), ha='center', va='bottom', fontsize=8, color="green")
  
   
 plt.legend()
